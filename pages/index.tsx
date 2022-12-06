02cce749462e94e7528481db1489acc861106ad9
SuchithG <suchithgowda1995@gmail.com>
2022-12-07 02:37:22 +0530

    feat: :art:

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="space-y-14 lg:space-y-24">
       <Head>
-        <title>Create Next App</title>
+        <title>Suchith.me</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="max-w-4xl mx-auto mt-16 antialiased">
