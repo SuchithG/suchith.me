6df1dc592fc7bc632c03ae11e50c6a62901060a3
SuchithG <suchithgowda1995@gmail.com>
2022-12-23 00:49:43 +0530

    feat: :art: Added AnimationOnScroll

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import "animate.css/animate.min.css";
 import AccordionUI from "./AccordionUI";
 
 const Accordion = () => {
@@ -23,21 +25,25 @@ const Accordion = () => {
   ];
 
   return (
-    <div className="flex flex-col justify-center items-center md:mt-32 md:mx-60 p-10 rounded-xl h-auto py-20 bg-gray-50">
-      <p className="font-semibold mb-8 text-black">Work experience</p>
-      {data.map((data) => {
-        return (
-          <AccordionUI
-            key={data}
-            title={data.question}
-            Id={data.id}
-            datas={data.answer}
-            Index={Index}
-            setIndex={setIndex}
-          ></AccordionUI>
-        );
-      })}
-    </div>
+    <AnimationOnScroll animateIn="animate__fadeIn">
+      <div className="flex flex-col justify-center items-center md:mt-32 md:mx-60 p-10 rounded-xl h-auto py-20 bg-gray-400">
+        <p className="font-semibold text-center mb-8 text-black">
+          Work experience
+        </p>
+        {data.map((data) => {
+          return (
+            <AccordionUI
+              key={data}
+              title={data.question}
+              Id={data.id}
+              datas={data.answer}
+              Index={Index}
+              setIndex={setIndex}
+            ></AccordionUI>
+          );
+        })}
+      </div>
+    </AnimationOnScroll>
   );
 };
 export default Accordion;
