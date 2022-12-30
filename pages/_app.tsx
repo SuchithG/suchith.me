d0f53fe6bf8807dc6156c9f9c22a6b08b5133ff7
SuchithG <suchithgowda1995@gmail.com>
2022-12-30 23:21:31 +0530

    refactor: :recycle: code re-arrange

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
+import { useRouter } from "next/router";
+import { ThemeProvider } from "next-themes";
 
 import Accordion from "../components/Accordion/Accordion";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
-import { useRouter } from "next/router";
-
 import * as ga from "../lib/ga";
 
 export default function App({ Component, pageProps }: AppProps) {
