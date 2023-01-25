8a81b28a3d263794a5e9cf9c2a0580ac54a703a2
SuchithG <suchithgowda1995@gmail.com>
2023-01-26 01:03:36 +0530

    feat: sample commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            Let&apos;s start building !🚀
+            Let&apos;s start building! 🚀
           </p>
           <h1 className="py-4">
             <motion.div
