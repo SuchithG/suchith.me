12adc18569dc69b56b83a31dc05dcfec454752a0
SuchithG <suchithgowda1995@gmail.com>
2023-08-17 17:25:05 +0530

    fix: :art: Text center alignement with more info button

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,19 +4,19 @@ import React from "react";
 
 const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex flex-col items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
-        className="rounded-xl group-hover:opacticy-10"
+        className="rounded-xl group-hover:opacity-10"
         src={backgroundImg}
         alt="/"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
+      <div className="hidden group-hover:flex flex-col items-center justify-center absolute inset-0z">
+        <h3 className="text-2xl text-white tracking-wider text-center mb-2">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">JavaScript</p>
+        <p className="text-white text-center mb-2">JavaScript</p>
         <Link href={projectUrl}>
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+          <p className="text-center py-1 px-3 rounded-lg bg-white text-gray-700 font-bold text-sm cursor-pointer">
             More Info
           </p>
         </Link>
